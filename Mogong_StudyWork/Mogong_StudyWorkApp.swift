2d1fefdd2b157349aa40ced214ea955db4c86174
Park Seo Yeon <syss220211@gmail.com>
2023-06-20 00:12:56 +0900

    Initial Commit

---
+++ Mogong_StudyWork/Mogong_StudyWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mogong_StudyWorkApp.swift
+//  Mogong_StudyWork
+//
+//  Created by 박서연 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct Mogong_StudyWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
